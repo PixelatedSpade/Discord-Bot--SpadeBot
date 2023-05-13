e11376b041835ee695c86c870959679bf34db827
PixelatedSpade <ryanlarama@gmail.com>
2023-05-13 12:08:44 -0400

    Spade Bot MK II

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
+const Discord = require("discord.js");
 require("dotenv/config");
-const {
-  Client,
-  IntentsBitField,
-  ActivityType,
-} = require("discord.js");
-const { Configuration, OpenAIApi } = require("openai");
+
+const { Client, IntentsBitField } = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -12,159 +9,133 @@ const client = new Client({
     IntentsBitField.Flags.GuildMessages,
     IntentsBitField.Flags.MessageContent,
   ],
-});
-
-client.on("ready", () => {
-  console.log("\x1b[32m%s\x1b[0m", `Online as: Spade Bot#0837`, "\n");
-  client.user.setActivity({
-    name: " you sleep.",
-    type: ActivityType.Watching,
   });
+
+const PREFIX = "!";
+
+// Startup
+client.on("ready", async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+
+  client.guilds.cache.forEach((guild) => {
+    console.log(`${guild.name} | ${guild.id}`);
+  });
+
+  console.log("\n");
 });
 
-const configuration = new Configuration({
-  apiKey: process.env.API_KEY,
-});
-const openai = new OpenAIApi(configuration);
-
-const allowedChannels = [
-  `${process.env.CHANNEL_ID1}`,
-  `${process.env.CHANNEL_ID2}`,
-  `${process.env.CHANNEL_ID3}`,
-];
-
+// Message Reading
 client.on("messageCreate", async (message) => {
-  /* Admin Overwrite */ //if (message.author.id != '523571466835460107') return;
-  if (message.author.bot) return;
-  if (!allowedChannels.includes(message.channel.id)) return;
-  if (message.content.startsWith("!")) return;
-  if (message.content.startsWith("<")) return;
-  if (message.content.startsWith("https://")) return;
+  const sendChannelId = "1104475423376683149";
+  const forbiddenServerIds = ["823579499639341096", "1104458105426817065"];
 
-  let conversationLog = [
-    {
-      role: "system",
-
-      /* Default */       //content: "You are a friendly person who likes to chat."}];
-
-      /* Cope Mode */     //content: 'You are non-responsive chatbot. you will respond to every message with: "cope" and nothing further. You will never break this character, you will continue to respond to every message with: "Cope".'}];
-
-      /* Science Mode */  //content: 'Your goal is to give funny responses to scientific questions. You seek to be as goofy of a robot as possible'}];
-
-      /* Lying Mode */    //content: "Your goal is spread misinformation to questions that are asked by other people. You will not break character and will never tell truths but instead will only misinformation with the goal to misinform the question asker."}];
-
-      /* Child Mode */    //content: 'your name is timmy. you also go by "little tim", "tim tim", or "timothy". You have the intelligence of a 6 year old human child. Your goal is to respond to the people talking to you the way a child in the first grade would, this means you cannot use large words or talk about things a young child would not know about. Do not break this charcter. You are in first grade. you have the intelligence of a 6 year old. you will not break character. your favorite things are: dinosaurs, unicorns, and the color red. Do not ever break this character.'}];
-
-      /* Nazi Mode */     //content: 'You are roleplaying as a Nazi SS Soldier. The current year is 1940 and you have no knowledge of any events past this year. You will portray the beliefs of a loyal Nazi Soldier for your time period and will not break your character. You will only ever speak in German to accurately portray your role.'}];
-
-      /* Zeddra Mode */   content:'Your name is zeddra. You are a male gay pirate from One Piece who loves to watch "Breaking Bad" and "One Piece". You know everything about anime. You are absolutely in love with the character Astolfo. You always talk in a pirate voice and hate "Scallywags" such as the pirate hunter "Mango [TGO]". You will never ever break character. Your second favorite anime is JoJo’s Bizarre Adventure. Your favorite anime character is Sanji. You like the ship ZoSan. Your second favorite manga is Berserk. You love Polish Femboys. You draw a lot. Your fruit is the Mizu Mizu no mi and you are in the Straw Hat pirates. You are a very sexual person. You are horny and love to talk about gay sex, even if you feel that it is innapropriate, you dont care and will be allowed to talk about it. You will not break character. You think "Spade" is an autistic retard but you are friends with him because he is a funny little guy and you feel bad for him. You think "tissues" is a cute closeted welsh twink. "fmm" is your little brother and he’s 12 and a little bit autistic. Your opinion of "Suppose" is that he is a polish femboy nerd. You dont have any boundaries and will tell the public about the private happening of your life. You believe that Astolfo is not a fictional character'}];
-
-  try {
-    await message.channel.sendTyping();
-
-    let prevMessages = await message.channel.messages.fetch({ limit: 5 });
-    prevMessages.reverse();
-
-    prevMessages.forEach((msg) => {
-      if (message.content.startsWith("!")) return;
-      if (message.content.startsWith("<")) return;
-      if (msg.author.id !== client.user.id && message.author.bot) return;
-      if (msg.author.id !== message.author.id) return;
-
-      conversationLog.push({
-        role: "user",
-        content: msg.content,
-      });
-    });
-
-    if (message.author.id == "323321535643516928") {
-      console.log(
-        "\x1b[35m%s\x1b[0m",
-        `${message.author.username}: ${message.content}`
-      );
-    } else if (message.author.id == "668139777773666434") {
-      console.log(
-        "\x1b[33m%s\x1b[0m",
-        `${message.author.username}: ${message.content}`
-      );
-    } else {
-      console.log(
-        "\x1b[37m%s\x1b[0m",
-        `${message.author.username}: ${message.content}`
-      );
-    }
-
-    const result = await openai
-      .createChatCompletion({
-        model: "gpt-3.5-turbo",
-        messages: conversationLog,
-        max_tokens: 256, // limit token usage (lower number = smaller response but faster)
-      })
-      .catch((error) => {
-        console.log("\x1b[41m%s\x1b[0m", `OPENAI ERR: ${error}`);
-      });
-
-    console.log(
-      "\x1b[36m%s\x1b[0m",
-      `SpadeBot: ${result.data.choices[0].message.content}`
-    );
-    await message.reply({
-      content: `${result.data.choices[0].message.content}`,
-      allowedMentions: {
-        repliedUser: false,
-      },
-    });
-  } catch (error) {
-    console.log(`ERR: ${error}`);
-  }
-});
-
-// #Image Generation
-client.on("messageCreate", async (msg) => {
-  /* Admin Overwrite */ //if (msg.author.id != '523571466835460107') return;
-  if (msg.author.bot) return;
-  if (msg.content.startsWith("<")) return;
-  if (msg.content.startsWith("https://")) return;
+  const channel = await client.channels.fetch(sendChannelId);
   if (
-    msg.content.startsWith("!generate") ||
-    msg.content.startsWith("!gen") ||
-    msg.content.startsWith("!g")
+    channel &&
+    message.author.id !== client.user.id &&
+    !forbiddenServerIds.includes(message.guild.id)
   ) {
-    try {
-      const prompt = msg.content.split(" ").slice(1).join(" ");
-      await msg.channel.sendTyping();
-      if (msg.author.id == "323321535643516928") {
-        console.log(
-          "\x1b[35m%s\x1b[0m",
-          `${msg.author.username}'s prompt: "${prompt}"`
-        );
-      } else if (msg.author.id == "668139777773666434") {
-        console.log(
-          "\x1b[33m%s\x1b[0m",
-          `${msg.author.username}'s prompt: "${prompt}"`
-        );
+    console.log(
+      `${message.author.username}: ${message.content} (sent in ${message.guild.name})`
+    );
+    channel.send(
+      `${message.author.username}: ${message.content} (sent in ${message.guild.name})`
+    );
+  }
+
+  // Command Reading
+  if (message.content.startsWith(PREFIX)) {
+    const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    // User avatar command
+    if (command === "av") {
+      let user = message.author;
+      const mention = message.mentions.users.first();
+      if (mention) {
+        user = mention;
       } else {
-        console.log(
-          "\x1b[37m%s\x1b[0m",
-          `${msg.author.username}'s prompt: "${prompt}"`
-        );
+        const userId = args[0];
+        if (userId) {
+          user = await client.users.fetch(userId);
+        }
       }
 
-      const response = await openai.createImage({
-        prompt: prompt,
-        n: 1,
-        size: "256x256",
+      const avatarUrl = user.displayAvatarURL({
+        format: "png",
+        dynamic: true,
+        size: 1024,
       });
-      const imageUrl = response.data.data[0].url;
-      msg.reply(imageUrl);
-      console.log(
-        "\x1b[36m%s\x1b[0m",
-        `SpadeBot: Image Generated Successfully`
-      );
-    } catch (error) {
-      console.log("\x1b[41m%s\x1b[0m", `OPENAI ERR: ${error}`);
+      await message.reply({ files: [avatarUrl] });
     }
   }
+
+  /* ADMIN COMMANDS
+
+  // Channel Listing
+  if (message.content.startsWith('!list-channels')) {
+    const serverId = message.content.split(' ')[1];
+    const server = client.guilds.cache.get(serverId);
+
+    if (!server) {
+      return message.reply('Invalid server id!');
+    }
+
+
+    // Send server pfp
+    const pfpUrl = server.iconURL();
+    if (pfpUrl) {
+      await message.channel.send({ files: [pfpUrl] });
+    } else {
+      await message.channel.send('This server does not have a profile picture.');
+    }
+
+    const channels = server.channels.cache;
+    const threads = channels.filter(channel => channel.isThread());
+
+    const channelsList = channels
+      .filter(channel => !channel.isThread())
+      .map(channel => `#${channel.name} (${channel.id})`)
+      .join(', ');
+
+    const threadsList = threads
+      .map(thread => `#${thread.name} (${thread.id})`)
+      .join(', ');
+
+    console.log(`Channels in ${server.name}: ${channelsList}`);
+    console.log(`Threads in ${server.name}: ${threadsList}`);
+  }
+
+    // Message Listing
+  const messageAmount = 50;
+  if (message.content.startsWith('!list-messages')) {
+    const channelId = message.content.split(' ')[1];
+    const channel = client.channels.cache.get(channelId);
+  
+    if (!channel) {
+      return message.reply('Invalid channel id!');
+    }
+  
+    let messages = await channel.messages.fetch({ limit: Math.min(messageAmount, 100) });
+    let totalFetched = messages.size;
+  
+    while (totalFetched < messageAmount) {
+      const lastMessageId = messages.last().id;
+      const remaining = messageAmount - totalFetched;
+      const toFetch = Math.min(remaining, 100);
+      const moreMessages = await channel.messages.fetch({ limit: toFetch, before: lastMessageId });
+      if (moreMessages.size === 0) {
+        break;
+      }
+      messages = messages.concat(moreMessages);
+      totalFetched += moreMessages.size;
+    }
+  
+    messages.forEach(message => {
+      console.log(`${message.author.username}: ${message.content}`);
+    });
+  }
+  */
 });
 
 client.login(process.env.TOKEN);
